9c66ff9d1df6d43464ead43e46ce70717712e58b
adeepta <mis23@mis.prangroup.com>
2025-02-03 17:57:03 +0600

    Created Model and Controller

---
+++ AutomatedQueryProcess/AutomatedQueryProcess/Models/QueryModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomatedQueryProcess.Models
+{
+    public class QueryModel
+    {
+        public string Query1 { get; set; }  
+        public string Query2 { get; set; }
+        public string Time { get; set; }
+    }
+}
