09399cbfce2f70f1a1f5cf37f2721e1656490df4
Adeepta Shushil <106776366+adeepta2000@users.noreply.github.com>
2025-02-04 23:22:50 +0600

    Try to set logger

--- AutomatedQueryProcess/AutomatedQueryProcess/Program.cs
+++ AutomatedQueryProcess/AutomatedQueryProcess/Program.cs
@@ -1,15 +1,33 @@
+using Serilog;
+using Serilog.Events;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+                       .ReadFrom.Configuration(configuration)
+                       .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Setup Serilog
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+);
+#endregion
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
